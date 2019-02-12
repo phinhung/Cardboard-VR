622d6be91a88a94df96538ef401f5d9a29d840ec
Nhung <ngnhung44@gmail.com>
2019-02-12 20:08:59 +0100

    Plugin eingefügt
    +"Hände" (Cubes)

---
+++ Assets/Scripts/PluginWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PluginWrapper : MonoBehaviour {
+
+    private AndroidJavaObject javaClass;
+    public Text myText;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+
+        javaClass = new AndroidJavaObject("com.example.vrlibrary.Keys");
+        javaClass.Call("LogNativeAndroidLogcatMessage");
+        javaClass.Call("LogNumberSentFromUnity", 76);
+
+		Debug.Log ("Juhu" + javaClass);
+
+        myText.text = javaClass.Call<int>("AddFiveToMyNumber", 4).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+
+
+	public void leiser(){
+		myText.text = "leiser";
+	}
+
+	public void lauter(){
+		myText.text = "lauter";
+	}
+
+
+
+}
