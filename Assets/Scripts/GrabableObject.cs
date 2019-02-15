3710ce4a4f8efb5c9c0ecc02e4f0ff51e8a23d3b
Nhung <ngnhung44@gmail.com>
2019-02-15 17:53:17 +0100

    Greifen eines Objektes, kein weiteres
    ansonsten müsste man noch irgendwie zwischen den Händen wählen, brauchen wir aber so jetzt nicht

---
+++ Assets/Scripts/GrabableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public GameObject player;
+	public GameObject hand;
+	public GameObject objecttograb;
+	public void setobject(){
+		if (hand.transform.childCount == 0) {
+			player.GetComponent<PluginWrapper> ().objectA = objecttograb.transform;
+		}
+	}
+}
--- Assets/Scripts/PluginWrapper.cs
+++ Assets/Scripts/PluginWrapper.cs
@@ -56,4 +56,6 @@ public class PluginWrapper : MonoBehaviour {
 		angeschaut = false;
 	}
 
+
+
 }
